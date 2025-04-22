372d6fabf5b07af1f14d9614f047dc59050cbc91
Harshaan Dhillon <harshaandsxf@gmail.com>
2025-04-22 21:13:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+import HomeScreen from './screens/HomeScreen.jsx'
+
+function App() {
+
+  return (
+    <HomeScreen />
+  )
+}
+
+export default App
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,15 @@
+function HomeScreen() {
+    return (
+        <div className="container-app">
+            <div className="div-header">
+                <h1>Start Workout</h1>
+            </div>
+
+
+            <div className=""></div>
+
+        </div>
+    )
+}
+
+export default HomeScreen
